2594f10c00656c7d5ffbbcef16a740eedceb7929
Noel Matta <noelmatta82@gmail.com>
2018-04-01 21:23:53 -0400

    functionality finished

--- dev/scripts/OutputMessage.js
+++ dev/scripts/OutputMessage.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 let currentDate = parseInt(new Date().getFullYear().toString().substr(2,2), 10);
 
+function message(messageChoice) {
+    switch(messageChoice) {
+        case '0': return null;
+        case '1': return `don't forget me when you're famous!`;
+        case '2': return `wish we hung out more!!`;
+        case '3': return `you will be successful for sure so I know who to call when I need a job (j/k)!`;
+        case '4': return `stay awesome!`;
+        case '5': return `party hard but leave some for me!!!`;
+    }
+}
+
 const OutputMessage = (props) => {
-    console.log(props);
     return (
         <div className="outputMessage">
             <p>Hey {props.name}{props.nickname ? ` aka ${props.nickname}` : '' }, class of '{currentDate - 1}/{currentDate} represent! The semester flew by so fast haha.</p>
             {props.course ? <p>{props.course} wouldn't have been the same without you.</p> : <p>Too bad we didn't have any classes together. </p> }
-            <p>But yeah, {props.message}!</p>
-            <p>Anywayz, {props.closing === true ? `let's chill sometime this summer ok? Don't be a stranger...` : `all the best!` }</p>
+            <p>But yeah, {message(props.message)}!</p>
+            <p>Anyway, {props.after === true ? `let's chill sometime this summer ok? Don't be a stranger...` : `all the best!` }</p>
             <p>{props.yourName}</p>
             {props.extraMessage ? <p>BTW, {props.extraMessage}</p> : null }
         </div>
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Questions from './Questions.js';
 import OutputMessage from './OutputMessage.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: 'Noel',
-      nickname: 'FenaxiZ',
-      course: 'Chemistry',
-      message: "don't forget me when you're famous",
-      closing: false,
-      yourName: 'Matt Eng',
-      extraMessage: 'test'
+      name: '',
+      nickname: '',
+      course: '',
+      message: 0,
+      after: false,
+      yourName: '',
+      extraMessage: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  toggle(e) {
+    // e.preventDefault();
+    this.setState({after: !this.state.after});
   }
   render() {
     return (
       <div>
-        <Questions />
-        <OutputMessage name={this.state.name} nickname={this.state.nickname} course={this.state.course} message={this.state.message} yourName={this.state.yourName} extraMessage={this.state.extraMessage} />
+        <form>
+          <label htmlFor="name">What's Their Name?</label>
+          <input type="text" id="name" value={this.state.name} onChange={this.handleChange} />
+          <label htmlFor="nickname">What's Their nickname (optional)?</label>
+          <input type="text" id="nickname" value={this.state.nickname} onChange={this.handleChange} />
+          <label htmlFor="course">What class did you have together?</label>
+          <input type="text" id="course" value={this.state.course} onChange={this.handleChange} />
+          <label htmlFor="message">Are they...</label>
+          <select id="message" onChange={this.handleChange}>
+            <option value="1">a rapper/singer/performer/athlete/etc.?</option>
+            <option value="2">that person you awkwardly had a crush on?</option>
+            <option value="3">really smart and hard-working?</option>
+            <option value="4">someone you honestly don't know?</option>            
+            <option value="5">the party animal type?</option>            
+          </select>
+          <label htmlFor="after">Do you still want to see this person?</label>
+          <input type="checkbox" onClick={this.toggle.bind(this)} />
+          <label htmlFor="yourName">Finally, What's Your Name?</label>
+          <input type="text" id="yourName" value={this.state.yourName} onChange={this.handleChange} />
+          <label htmlFor="extraMessage">Anything else you want to say? (optional)</label>
+          <textarea type="text" id="extraMessage" value={this.state.extraMessage} onChange={this.handleChange} />
+        </form>
+        <OutputMessage name={this.state.name} nickname={this.state.nickname} course={this.state.course} message={this.state.message} yourName={this.state.yourName} after={this.state.after} extraMessage={this.state.extraMessage} />
       </div>
     )
   }
