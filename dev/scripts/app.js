b7f25f2ec8a72f7f445da1bde6347c7efd0db442
Noel Matta <noelmatta82@gmail.com>
2018-04-02 00:29:41 -0400

    finished except media breakpoints

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -27,31 +27,37 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
-        <form>
-          <label htmlFor="name">What's Their Name?</label>
-          <input type="text" id="name" value={this.state.name} onChange={this.handleChange} />
-          <label htmlFor="nickname">What's Their nickname (optional)?</label>
-          <input type="text" id="nickname" value={this.state.nickname} onChange={this.handleChange} />
-          <label htmlFor="course">What class did you have together?</label>
-          <input type="text" id="course" value={this.state.course} onChange={this.handleChange} />
-          <label htmlFor="message">Are they...</label>
-          <select id="message" onChange={this.handleChange}>
-            <option value="1">a rapper/singer/performer/athlete/etc.?</option>
-            <option value="2">that person you awkwardly had a crush on?</option>
-            <option value="3">really smart and hard-working?</option>
-            <option value="4">someone you honestly don't know?</option>            
-            <option value="5">the party animal type?</option>            
-          </select>
-          <label htmlFor="after">Do you still want to see this person?</label>
-          <input type="checkbox" onClick={this.toggle.bind(this)} />
-          <label htmlFor="yourName">Finally, What's Your Name?</label>
-          <input type="text" id="yourName" value={this.state.yourName} onChange={this.handleChange} />
-          <label htmlFor="extraMessage">Anything else you want to say? (optional)</label>
-          <textarea type="text" id="extraMessage" value={this.state.extraMessage} onChange={this.handleChange} />
-        </form>
+      <main>
+        <section class="intro">
+          <p>Damnit, someone just asked you to sign their yearbook and you don't know what to write. Don't worry... just fill out the form below to get a quick message you can totally use!</p>
+        </section>
+        <div className="questions">
+          <form>
+            <label htmlFor="name">What's Their Name?</label>
+            <input type="text" id="name" value={this.state.name} onChange={this.handleChange} />
+            <label htmlFor="nickname">What's Their Nickname (optional)?</label>
+            <input type="text" id="nickname" value={this.state.nickname} onChange={this.handleChange} />
+            <label htmlFor="course">What Class Did You Have Together?<br />(leave blank if none)</label>
+            <input type="text" id="course" value={this.state.course} onChange={this.handleChange} />
+            <label htmlFor="message">Are They...</label>
+            <select id="message" onChange={this.handleChange}>
+              <option value="1">a rapper/singer/performer/athlete/etc.?</option>
+              <option value="2">that person you awkwardly had a crush on?</option>
+              <option value="3">really smart and hard-working?</option>
+              <option value="4">someone you honestly don't know?</option>            
+              <option value="5">the party animal type?</option>            
+            </select>
+            <label name="after">Do You Still Want To See This Person?</label>
+            <input type="checkbox" onClick={this.toggle.bind(this)} />
+            <label name="yourName">Finally, What's Your Name?</label>
+            <input type="text" id="yourName" value={this.state.yourName} onChange={this.handleChange} />
+            <label htmlFor="extraMessage">Anything else you want to say? (optional)</label>
+            <textarea type="text" id="extraMessage" value={this.state.extraMessage} onChange={this.handleChange} />
+          </form>
+          <h2>Cool! Here's your message:</h2>
+        </div>
         <OutputMessage name={this.state.name} nickname={this.state.nickname} course={this.state.course} message={this.state.message} yourName={this.state.yourName} after={this.state.after} extraMessage={this.state.extraMessage} />
-      </div>
+      </main>
     )
   }
 }
